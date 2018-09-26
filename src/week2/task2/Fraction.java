998f67a16358131cdbbae055fd8320c0dd40e57b
huynh1234 <42998636+huynh1234@users.noreply.github.com>
2018-09-26 18:40:15 +0700

    Update Fraction.java

--- src/week2/task2/Fraction.java
+++ src/week2/task2/Fraction.java
@@ -1,88 +1,157 @@
-package week2.task2;
+    package week2.task2;
+
+
+//import java.util.Scanner;
 
 public class Fraction {
-    private int numerator,denominator;
-    public void SetNumerator(int a){
-        numerator=a;
+
+    // TODO: khai b�o c�c thu?c t�nh
+    private int numerator;
+    private int denominator;
+
+    public Fraction() {
+
     }
-    public int setNumerator(){
+
+    //ham tinh uoc trung lon nhat!
+    public static int gcd(int a, int b) {
+
+        int c = 0;
+        if(a<0){
+            a=-a;
+        }
+        for (int i = 1; i <= a; i++) {
+            if (a % i == 0 && b % i == 0) {
+                c = i;
+            }
+        }
+        return c;
+    }
+
+    public int getNumerator() {
         return numerator;
     }
-    public void getDenominator(int b){
-        denominator=b;
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
     }
-    public int getDenominator(){
+
+    public int getDenominator() {
         return denominator;
     }
 
-    // TODO: khai báo các thuộc tính
+    public void setDenominator(int denominator) {
+        this.denominator = denominator;
+    }
 
     public Fraction(int numerator, int denominator) {
-        this.numerator=numerator;
-        this.denominator=denominator;
-        // TODO: khởi tạo giá trị cho các thuộc tính numberator (tử số), denominator (mẫu số)
+        // TODO: kh?i t?o gi� tr? cho c�c thu?c t�nh numberator (t? s?), denominator (m?u s?)
+        this.numerator = numerator;
+        this.denominator = denominator;
     }
 
     public Fraction add(Fraction other) {
-        Fraction sum = new Fraction(1,1);
-        sum.numerator = this.numerator*other.denominator+other.numerator*this.denominator;
-        sum.denominator=this.denominator*other.denominator;
-        System.out.println(sum.numerator + "/" + sum.denominator);
-        return sum;
-        // TODO: Phương thức cộng hai phân số (this và other), trả về đối tượng Fraction mới
+        Fraction res = new Fraction();
+        res.numerator = other.denominator * this.numerator + this.denominator * other.numerator;
+        res.denominator = this.denominator * other.denominator;
+        if (res.numerator != 0) {
+            int c = gcd(res.numerator, res.denominator);
+            res.denominator = res.denominator / c;
+            res.numerator = res.numerator / c;
+        }
 
+        // TODO: Ph��ng th?c c?ng hai ph�n2 5 s? (this v� other), tr? v? �?i t�?ng Fraction m?i
+        return res;
     }
 
     public Fraction subtract(Fraction other) {
-        Fraction subtract = new Fraction (1,1);
-        subtract.numerator=this.numerator*other.denominator-other.numerator*this.denominator;
-        subtract.denominator=this.denominator*other.denominator;
-        System.out.println(subtract.numerator + "/" + subtract.denominator);
-        return subtract;
-
-        // TODO: Phương thức trừ hai phân số (this và other), trả về đối tượng Fraction mới
+        // TODO: Ph��ng th?c tr? hai ph�n s? (this v� other), tr? v? �?i t�?ng Fraction m?i
+        Fraction res = new Fraction();
+        res.numerator = other.denominator * this.numerator - this.denominator * other.numerator;
+        res.denominator = this.denominator * other.denominator;
+        if (res.numerator != 0) {
+            int c = gcd(res.numerator, res.denominator);
+            res.denominator = res.denominator / c;
+            res.numerator = res.numerator / c;
+        }
+        return res;
     }
 
     public Fraction multiply(Fraction other) {
-        Fraction multiply = new Fraction(1,1);
-        multiply.numerator=this.numerator*other.numerator;
-        multiply.denominator=this.denominator*other.denominator;
-        System.out.println(multiply.numerator + "/" + multiply.denominator);
-        return multiply;
-        // TODO: Phương thức nhân hai phân số (this và other), trả về đối tượng Fraction mới
-
+        Fraction res = new Fraction();
+        res.denominator = this.denominator * other.denominator;
+        res.numerator = this.numerator * other.numerator;
+        if (res.numerator != 0) {
+            int c = gcd(res.numerator, res.denominator);
+            res.denominator = res.denominator / c;
+            res.numerator = res.numerator / c;
+        }
+        // TODO: Ph��ng th?c nh�n hai ph�n s? (this v� other), tr? v? �?i t�?ng Fraction m?i
+        return res;
     }
 
     public Fraction divide(Fraction other) {
-        Fraction divide=new Fraction(1,1);
-        divide.numerator=this.numerator*other.denominator;
-        divide.denominator=this.denominator*other.numerator;
-        System.out.println(divide.numerator + "/" + divide.denominator);
+        // TODO: Ph��ng th?c chia hai ph�n s? (this v� other), tr? v? �?i t�?ng Fraction m?i
+        Fraction res = new Fraction();
+        res.denominator = this.denominator * other.numerator;
+        res.numerator = this.numerator * other.denominator;
+        if (res.numerator != 0) {
+            int c = gcd(res.numerator, res.denominator);
+            res.denominator = res.denominator / c;
+            res.numerator = res.numerator / c;
+        }
+        return res;
+    }
 
-        return divide;
-        // TODO: Phương thức chia hai phân số (this và other), trả về đối tượng Fraction mới
+    public boolean equals(Fraction other) {
+        Fraction res = new Fraction();
+        res.denominator = this.denominator * other.numerator;
+        res.numerator = this.numerator * other.denominator;
+
+        if (res.denominator == res.numerator) {
+            return true;
+        } else {
+            return false;
+
+        }
 
     }
 
-    public boolean equals(Object obj){
-        Fraction a = (Fraction) obj;
-        return (numerator*a.denominator == a.numerator*denominator);
-
+    @Override
+    public String toString() {
+        return numerator + "/" + denominator;
     }
+    public static void main(String[] args) {
+        Fraction a = new Fraction(2, 5);
+        Fraction b= new Fraction(2,5);
+        System.out.println("tong hai phan so la :" + a.add(b).toString());
+        System.out.println("hieu hai phan so la : " + a.subtract(b).toString());
+        System.out.println("tich hai phan so la : " + a.multiply(b).toString());
+        System.out.println("thuong hai phan so la : " + a.divide(b).toString());
+        if (a.equals(b)) {
+            System.out.println("hai phan so bang nhau");
+        } else {
+            System.out.println("hap phan so khac nhau");
+        }
+        /*
+        Fraction a = new Fraction();//   Fraction b = new Fraction();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap A: ");
+        a.numerator = sc.nextInt();
+        a.denominator = sc.nextInt();
+        System.out.println("nhap B:");
+        b.numerator = sc.nextInt();
+        b.denominator = sc.nextInt();
+        System.out.println("tong hai phan so la :" + a.add(b).toString());
+        System.out.println("hieu hai phan so la : " + a.subtract(b).toString());
+        System.out.println("tich hai phan so la : " + a.multiply(b).toString());
+        System.out.println("thuong hai phan so la : " + a.divide(b).toString());
+        if (a.equals(b)) {
+            System.out.println("hai phan so bang nhau");
+        } else {
+            System.out.println("hap phan so khac nhau");
+        }
+        */
+        }
 
-    public static void main(String[] args){
-        Fraction a = new Fraction(12,34);
-        Fraction b = new Fraction(2,9);
-        Fraction c = new Fraction(1,1);
-
-
-
-       //*result = a.subtract(b);
-       // System.out.println("ket qua phep tru la :" +result);
-       // result = a.multiply(b);
-      //  System.out.println("ket qua phep nhan la :" +result);
-      //  result = a.divide(b);
-      //  System.out.println("ket qua phep chia la :"+result);
-      //  System.out.println("hai phan so bang nhau :" +a.equals(b));
-    }
 }
