bd13f622e454aad01ec427105dca3d71f6c4f346
huynh1234 <42998636+huynh1234@users.noreply.github.com>
2018-09-26 18:40:54 +0700

    Update Task1.java

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -1,33 +1,55 @@
-package week2.task1;
+package week2.Task1;
 
-import java.util.Scanner;
+//import java.util.Scanner;
+
 
 public class Task1 {
-        public static int gcd(int a, int b) {
-        if (b == 0) return a;
-        return gcd(b, a % b);
 
-
-        // TODO: Tính ước chung lớn nhất của 2 số a, b
+    public static int gcd(int a, int b) {
+        int c=0;
+        if(a<0){
+            a=-a;
         }
+        for(int i=1;i<=a;i++){
+            if(a%i==0&&b%i==0){
+                c=i;
+            }
+        }
+        return c;
+    }
+
+    public static int test(int a){
+        return 1;
+    }
 
     public static int fibonacci(int n) {
-            if (n==0 ) return 0;
-            else if (n==1) return 1;
-            else  {
-                return fibonacci(n-1)+fibonacci(n-2);
-            }
-        // TODO: Tìm số fibonacci ở vị trí n
-            }
-    public static void main(String[] args){
-        int a=6;
-        int b=9;
-        System.out.println("Uoc chung lon nhat la "+gcd(a,b));
-        for (int i=0; i<31; i++){
-            System.out.println( +fibonacci(i)+" ");
-        }
-
-
-
+        // TODO: T?m s? fibonacci ? v? tr� n
+       if(n==0)
+           return 0;
+       else if(n==1)
+           return 1;
+       else
+           return (fibonacci(n-1)+fibonacci(n-2));
     }
+
+    public static void main(String[] args) {
+        int a=3,b=6;
+        System.out.println("uoc trung lon nhat la: "+ gcd(a,b) );
+        System.out.println("fibonac"+fibonacci(a));
+        /*
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap A: ");
+        a= sc.nextInt();
+        System.out.println("nhap B:");
+       b= sc.nextInt();
+
+
+       System.out.println("uoc trung lon nhat la: "+ gcd(a,b) );
+       System.out.println(fibonacci(a));
+       */
+    }
+
+
+
 }
