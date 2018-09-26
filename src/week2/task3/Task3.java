3035648a96ecb972f93b73c368cabfa4df79f17f
Sonsieg <43144899+Sonsieg@users.noreply.github.com>
2018-09-26 15:48:47 +0700

    Update Task3.java

--- src/week2/task3/Task3.java
+++ src/week2/task3/Task3.java
@@ -1,6 +1,102 @@
 package week2.task3;
 
 public class Task3 {
-}
+    class Student{
+        String name;
+        int age;
+        String email;
+        public String getName()
+        {
+            return this.name;
+        }
+        public void setName(String name) {
+            this.name=name;
+        }
+        public int getAge()
+        {
+            return this.age;
+        }
 
-//TODO: khai báo 3 class tương ứng với 3 đối tượng thực tế ở dưới
+        public void setAge(int age) {
+            this.age = age;
+        }
+        public String getEmail()
+        {
+            return this.email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+        // TODO hoc sinh
+        public void Name()
+        {
+            System.out.println("ten toi la"+ this.name);
+        }
+        // TODO hoc sinh dang code
+        public void Study()
+        {
+            System.out.println("I am coding");
+        }
+        // TODO mon tin hoc
+        public void Subject()
+        {
+            System.out.println("tin hoc");
+        }
+    }
+    // TODO doi tuong Student
+}
+    class friend{
+        String name;
+        int age;
+        String email;
+        public String getName()
+        {
+            return this.name;
+        }
+        public void setName(String name) {
+            this.name=name;
+        }
+        public int getAge()
+        {
+            return this.age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+        public String getEmail()
+        {
+            return this.email;
+        }
+        public void School() { System.out.println("UET"); }
+        public void Class() {   System.out.println("K62IE4");}
+        public void Email() { System.out.println("maxseo9x@gmail.com") ;}
+    }
+    class mylove {
+        String name;
+        int age;
+        String job;
+        public String getName()
+        {
+            return this.name;
+        }
+        public void setName(String name) {
+            this.name=name;
+        }
+        public int getAge()
+        {
+            return this.age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+        public String getJob()
+        {
+            return this.job;
+        }
+        public void School() { System.out.println("THPT NG"); }
+        public void Class() {   System.out.println("12DK50");}
+        public void Job() { System.out.println("Student") ;}
+    }
