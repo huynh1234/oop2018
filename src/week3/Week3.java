3eb7b66fbc66d46952d53b63a1d97bf35686347f
Sonsieg <43144899+Sonsieg@users.noreply.github.com>
2018-10-03 00:02:40 +0700

    Update Week3.java

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -1,25 +1,41 @@
-package week3;
-
 public class Week3 {
 
     public static int max(int m, int n) {
-        // TODO: Tìm giá trị lớn nhất của hai số nguyên, giá trị trả về của hàm là số lớn nhất
-        return -1;
+
+        if(m>=n) return m;
+        else return n;
     }
 
     public static int minOfArray(int[] array) {
-        // TODO: Tìm giá trị nhỏ nhất của của một mảng số nguyên (kích thước mảng <= 100 phần tử)
-        return -1;
+
+        int min=array[0];
+        for(int i=1;i<array.length;i++){
+            if(min>array[i]){
+                min=array[i];
+            }
+        }
+
+        return min;
     }
 
-    /**
-     * Chương trình tính chỉ số BMI và in ra kết quả đánh giá
-     * @param weight cân nặng
-     * @param height chiều cao
-     * @return Thiếu cân, Bình thường, Thừa cân, Béo phì
-     */
     public static String calculateBMI(double weight, double height) {
-        // TODO: Viết chương trình tính chỉ số BMI và in ra kết quả đánh giá
-        return null;
+
+        double BMI= (weight/(height*height));
+        if(BMI>=0 && BMI< 18.5) return "Thieu can";
+        else if(BMI>=18.5 && BMI<23) return "Binh thuong";
+        else if(BMI>=23 && BMI<=24.99) return "Thua can";
+        else if(BMI>25) return "Beo phi";
+	return null;
+    }
+    public static void main(String[] args){
+        int [] mang = new int[5];
+         mang[0]=3;
+         mang[1]=10;
+         mang[2]=1;
+         mang[3]=19;
+         mang[4]=99;
+        System.out.println(max(3,10));
+        System.out.println(minOfArray(mang));
+        System.out.println(calculateBMI(55,1.69));
     }
 }
