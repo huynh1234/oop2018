de1b25ac44a03899764bb56f4f7d353e94c0a238
Sonsieg <43144899+Sonsieg@users.noreply.github.com>
2018-10-03 15:48:31 +0700

    Update Week3Test.java

--- test/week3/Week3Test.java
+++ test/week3/Week3Test.java
@@ -2,41 +2,144 @@ package week3;
 
 import org.junit.Assert;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class Week3Test {
 
     // TODO: Viết 5 testcase cho phương thức max()
     @Test
-    public void testMax(){
-        Assert.assertEquals(11,Week3.max(11,-1));
-        Assert.assertEquals(6,Week3.max(4,6));
-        Assert.assertEquals(7,Week3.max(7,3));
-        Assert.assertEquals(10,Week3.max(2,10));
-        Assert.assertEquals(0,Week3.max(-3,0));
+    public void testMax1(){
+        int a=10;
+        int b=9;
+        int c= Week3.max(a, b);
+       assertEquals(c, a);
     }
+    @Test
+    public void testMax2(){
+        int a=7;
+        int b=8;
+        int c = Week3.max(a, b);
 
+        assertEquals(c, b);
+    }
+    @Test
+    public void testMax3(){
+        int a=5;
+        int b=6;
+        int c = Week3.max(a, b);
+
+        assertEquals(c, b);
+    }
+    @Test
+    public void testMax4(){
+        int a=3;
+        int b=4;
+        int c = Week3.max(a, b);
+
+        assertEquals(c, b);
+    }
+    @Test
+    public void testMax5(){
+        int a=2;
+        int b=1;
+        int c = Week3.max(a, b);
+
+        assertEquals(c, a);
+    }
+    
     // TODO: Viết 5 testcase cho phương thức minOfArray()
     @Test
-    public void testMinOfArray(){
-        int[] mang1 = {5,5,9,4,-1};
-        Assert.assertEquals(-1,Week3.minOfArray(mang1));
-        int[] mang2 = {-2,7,9,45,9};
-        Assert.assertEquals(-2,Week3.minOfArray(mang2));
-        int[] mang3 = {-3,5,0,2,4};
-        Assert.assertEquals(-3, Week3.minOfArray(mang3));
-        int[] mang4 = {-4,-1,9,5,6};
-        Assert.assertEquals(-4,Week3.minOfArray(mang4));
-        int[] mang5 = {2,-5,6,11,0};
-        Assert.assertEquals(-5,Week3.minOfArray(mang5));
-    }
+    public void testMin0fArray1(){
+        int[] mang = new int[100];
+        mang[0] = 5;
+        mang[1] = 4;
+        mang[2] = 3;
+        mang[3] = 2;
 
+        int min = Week3.minOfArray(mang);
+        assertEquals(min, 2);
+    }
+    @Test
+    public void testMin0fArray2(){
+        int[] mang = new int[100];
+        mang[0] = 6;
+        mang[1] = 10;
+        mang[2] = 3;
+        mang[3] = 5;
+
+        int min = Week3.minOfArray(mang);
+        assertEquals(min, 3);
+    }
+    @Test
+    public void testMin0fArray3(){
+        int[] mang = new int[100];
+        mang[0] = 9;
+        mang[1] = 4;
+        mang[2] = 2;
+        mang[3] = 10;
+
+        int min = Week3.minOfArray(mang);
+        assertEquals(min, 2);
+    }
+    @Test
+    public void testMin0fArray4(){
+        int[] mang = new int[100];
+        mang[0] = 9;
+        mang[1] = 5;
+        mang[2] = 2;
+        mang[3] = 0;
+
+        int min = Week3.minOfArray(mang);
+        assertEquals(min, 0);
+    }
+    @Test
+    public void testMin0fArray5(){
+        int[] mang = new int[100];
+        mang[0] = 99;
+        mang[1] = 11;
+        mang[2] = 66;
+        mang[3] = 10;
+
+        int min = Week3.minOfArray(mang);
+        assertEquals(min, 10);
+    }
     // TODO: Viết 5 testcase cho phương thức calculateBMI()
     @Test
-    public void testCalculateBMI(){
-        Assert.assertEquals("Thieu can",Week3.calculateBMI(30,1.3));
-        Assert.assertEquals("Binh thuong",Week3.calculateBMI(40,1.4));
-        Assert.assertEquals("Binh thuong",Week3.calculateBMI(50,1.5));
-        Assert.assertEquals("Thua can",Week3.calculateBMI(60,1.6));
-        Assert.assertEquals("Thua can",Week3.calculateBMI(70,1.7));
+    public void testCalculateBMI1(){
+        double weight = 30;
+        double height = 1.3;
+        String BMI = Week3.calculateBMI(weight, height);
+        assertEquals(BMI, "Thieu can");
     }
+    
+    @Test
+    public void testCalculateBMI2(){
+        double weight = 40;
+        double height = 1.4;
+        String BMI = Week3.calculateBMI(weight, height);
+        assertEquals(BMI, "Binh thuong");
+    }
+    @Test
+    public void testCalculateBMI3(){
+        double weight = 50;
+        double height = 1.5;
+        String BMI = Week3.calculateBMI(weight, height);
+        assertEquals(BMI, "Binh thuong");
+    }
+    @Test
+    public void testCalculateBMI4(){
+        double weight = 60;
+        double height = 1.6;
+        String BMI = Week3.calculateBMI(weight, height);
+        assertEquals(BMI, "Thua can");
+    }
+    @Test
+    public void testCalculateBMI5(){
+        double weight = 70;
+        double height = 1.7;
+        String BMI = Week3.calculateBMI(weight, height);
+        assertEquals(BMI, "Thua can");
+    }
+
+
 }
