f65c0e095e4e728819786a8e7521916dc92d21a2
Sonsieg <43144899+Sonsieg@users.noreply.github.com>
2018-10-08 21:12:39 +0700

    Update Student.java

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -2,84 +2,66 @@ package week1;
 
 public class Student {
 
-    // TODO: khai báo các thuộc tính cho Student
-    private String id;
     private String name;
+    private String id;
     private String group;
     private String email;
-    // TODO: khai báo các phương thức getter, setter cho Student
-
-    /**
-     * Constructor 1
-     */
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
 
     public String getName() {
-        return name;
+        return this.name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setName(String n) {
+        this.name = n;
+    }
+    public String getId() {
+        return this.id;
     }
 
+    public void setId(String ids) {
+        this.id = ids;
+    }
     public String getGroup() {
-        return group;
+        return this.group;
     }
 
-    public void setGroup(String group) {
-        this.group=group;
+    public void setGroup(String gr) {
+        this.group = gr;
     }
-    public void setEmail(String email){
-        this.email=email;
+    public void setEmail(String em) {
+        this.email = em;
     }
-    public String getEmail(){
-        return email;
+    public String getEmail() {
+        return this.email;
     }
+
+
     Student() {
+        this.name = "Student";
+        this.id = "000";
+        this.group = "INT22041";
+        this.email = "uet@vnu.edu.vn";
 
-        name ="Student";
-        id="000";
-        group="INT22041";
-        email="uet@vnu.edu.vn";
     }
 
-    /**
-     * Constructor 2
-     * @param n
-     * @param sid
-     * @param em
-     */
     Student(String n, String sid, String em) {
-        this.name=n;
-        this.id=sid;
-        this.email=em;
-        this.group="INT22041";
+        this.name = n;
+        this.id = sid;
+        this.group = "INT22041";
+        this.email = em;
 
     }
 
-    /**
-     * Constructor 3
-     * @param s
-     */
     Student(Student s) {
-        this.name=s.name;
-        this.id=s.id;
-        this.email=s.email;
-        this.group=s.group;
+        this.name = s.name;
+        this.id = s.id;
+        this.group = s.group;
+        this.email = s.email;
 
     }
 
     String getInfo() {
-        System.out.println(name);
-        System.out.println(id);
-        System.out.println(group);
-        System.out.println(email);
-        return null; // xóa dòng này sau khi cài đặt
+        String TTSV ="Name: "+ name + "\nID: " + id + "\nGroup: " + group + "\nEmail: " + email + "\n";
+        return TTSV;
     }
 }
