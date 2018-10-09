50664e50ee7c54df5dfe819f4b12958daedaecea
Sonsieg <43144899+Sonsieg@users.noreply.github.com>
2018-10-09 22:57:16 +0700

    Update Task1.java

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -1,25 +1,32 @@
 package week2.task1;
+
 public class Task1 {
 
     public static int gcd(int a, int b) {
-        int tcm;
-        while(b!=0){
-            tcm = a%b;
-            a = b;
-            b = tcm;
+        // TODO: Tính ước chung lớn nhất của 2 số a, b
+        int uc = 0;
+        for (int i = 1 ; i <= a || i <= b ; i++){
+            if( a % i == 0 && b % i == 0)
+                uc = i;
         }
-        return a;
-
+        return uc;
     }
 
     public static int fibonacci(int n) {
-        if(n==1||n==0) return n;
-        return fibonacci(n-1)+fibonacci(n-2);
+        // TODO: Tìm số fibonacci ở vị trí n
+        if (n == 0)
+            return 0;
+        else return (n == 1)?1:(fibonacci(n-1)+fibonacci(n-2));
     }
+
     public static void main(String[] args) {
-        int a= 4;
-        int b=6;
-        System.out.println(gcd(a,b));
-        System.out.println(fibonacci(5));
+        int x = 6;
+        int y = 9;
+        int uc = gcd(x,y);
+        System.out.println(uc);
+
+        int a = 0;
+        int fi = fibonacci(a);
+        System.out.println(fi);
     }
 }
