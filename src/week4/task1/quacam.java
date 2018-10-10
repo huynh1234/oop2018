e8511a4deb9088f4b8b229ab4f69d4d551c7f74c
huynh1234 <42998636+huynh1234@users.noreply.github.com>
2018-10-10 18:37:13 +0700

    Update quacam.java

--- src/week4/task1/quacam.java
+++ src/week4/task1/quacam.java
@@ -1,1 +1,28 @@
+package week4.task1;
+
+public class quacam extends hoaqua{ // cam cao phong ke thua hoa qua
+    private String mui;
+    public quacam(){
+        this.mui=mui;
+    }
+    public quacam(String mui){
+        this.mui="";
+    }
+    public String getmui(){
+        return mui;
+    }
+    public void setmui(String mui){
+        this.mui=mui;
+    }
+    public void quacamnhuthenao(camcaophong sc){
+        System.out.print("Cam Cao Phong co ten "+ sc.getten());
+        System.out.print("Cam Cao Phong nhap tai "+ sc.getmadin());
+        System.out.print("so luong Cam Cao Phong  "+ sc.getsoluong());
+        System.out.print("Cam Cao Phong co khoi luong "+ sc.getmui());
+    }
+
+
+
+}
+
 
