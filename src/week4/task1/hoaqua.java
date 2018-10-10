40d15ef086c33e87f5b0eed4318ce997824ffe8b
huynh1234 <42998636+huynh1234@users.noreply.github.com>
2018-10-10 18:35:20 +0700

    Update hoaqua.java

--- src/week4/task1/hoaqua.java
+++ src/week4/task1/hoaqua.java
@@ -1,1 +1,40 @@
+package week4.task1;
+public class hoaqua{
+    private String ten;
+    private String madin;
+    private int soluong;
+
+    public hoaqua(){
+        this.ten="";
+        this.madin="";
+        this.soluong=0;
+    }
+    public hoaqua(String ten,String m,int s){
+        this.ten=ten;
+        this.madin=m;
+        this.soluong=s;
+    }
+    public String getten(){
+             return ten;
+         }
+    public void setten(String ten){
+            this.ten = ten;
+     }
+    public String getmadin(){
+        return madin;
+    }
+    public void setmadine(String madin){
+        this.madin = madin;
+    }
+
+    public String getsoluong(){
+        return ten;
+    }
+    public void setsoluong(int  soluong){
+        this.soluong = soluong;
+    }
+
+
+
+}
 
