23edfac9e3a7450ce9ff6add13af06d92c8e4713
huynh1234 <42998636+huynh1234@users.noreply.github.com>
2018-10-10 18:38:34 +0700

    Update quatao.java

--- src/week4/task1/quatao.java
+++ src/week4/task1/quatao.java
@@ -1,1 +1,28 @@
 
+package week4.task1;
+
+public class quatao extends hoaqua{ // cam cao phong ke thua hoa qua
+        private String vi;
+    public quatao(){
+            this.vi=vi;
+        }
+    public quatao(String vi){
+            this.vi="";
+        }
+    public String getvi(){
+        return vi;
+    }
+    public void setquatao(String vi){
+        this.vi=vi;
+    }
+    public void quataonhuthenao(quatao sc){
+        System.out.print("Qua Tao co ten "+ sc.getten());
+        System.out.print("Qua tao nhap tai "+ sc.getmadin());
+        System.out.print("khoi luong Tao "+ sc.getsoluong());
+        System.out.print("Qua Tao co vi"+ sc.getvi());
+    }
+
+
+
+}
+
