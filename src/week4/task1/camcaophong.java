ba6f72e7d1b56e68f1cd19a164f16feafe77624c
huynh1234 <42998636+huynh1234@users.noreply.github.com>
2018-10-10 18:48:12 +0700

    Update camcaophong.java

--- src/week4/task1/camcaophong.java
+++ src/week4/task1/camcaophong.java
@@ -1,8 +1,19 @@
-
 package week4.task1;
 
 public class camcaophong extends quacam{ // cam cao phong ke thua hoa qua
-
+    private String mui;
+    public camcaophong(){
+        this.mui=mui;
+    }
+    public camcaophong(String mui){
+        this.mui="";
+    }
+    public String getmui(){
+        return mui;
+    }
+    public void setmui(String mui){
+        this.mui=mui;
+    }
     public void camcaophongnhuthenao(camcaophong sc){
         System.out.print("Cam Cao Phong co ten "+ sc.getten());
         System.out.print("Cam Cao Phong nhap tai "+ sc.getmadin());
